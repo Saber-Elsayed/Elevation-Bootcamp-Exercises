a1b86ddad86b385c17f31e7b10ec159f78b7d5d3
Saber-Elsayed <alsayyedsabe@gmail.com>
2023-12-03 19:23:53 +0200

    handle project

---
+++ Week-4/Handlebars project/main.js
@@ -0,0 +1,24 @@
+
+$("button").on("click", function () {
+    let address = $("#addr-input").val()
+    let minPrice = $("#min-p-input").val()
+    let maxPrice = $("#max-p-input").val()
+    let minRooms = $("#min-r-input").val()
+    let maxRooms = $("#max-r-input").val()
+    let immediate = $("#immed-y")
+
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    renderApts(relevantApts)
+})
+const source = $('#results').html();
+const template = Handlebars.compile(source);
+const renderApts = function (apartments) {
+    console.log(JSON.stringify(apartments))
+    $("#results").empty()
+    let newHTML = template(apartments);
+    $("#results").append(newHTML)
+    console.log(apartments) //array of apartments to render
+    //Your code goes here.
+}
+
+renderApts(apartments) //renders apartments when page loads
