10a757d44684c5c42e15cda9fcabe2c4b4f5eb33
Saber-Elsayed <alsayyedsabe@gmail.com>
2023-12-28 13:29:06 +0200

    promise exercise

---
+++ Week-6/main.js
@@ -0,0 +1,66 @@
+function bakeCake() {
+  console.log("Starting the cake baking process...");
+
+  const batter = prepareBatter();
+  const cake = bakeBatter(batter);
+  cake.then((cake) => {
+    const frostedCake = frostCake(cake);
+
+    console.log("Cake baking process completed!");
+    console.log("Final Cake:", JSON.stringify(frostedCake));
+  });
+}
+
+function prepareBatter() {
+  console.log("Preparing cake batter...");
+  const flour = measureIngredients("flour", 2);
+  const sugar = measureIngredients("sugar", 1);
+  const eggs = measureIngredients("eggs", 3);
+  const butter = measureIngredients("butter", 0.5);
+  const batter = mixIngredients([flour, sugar, eggs, butter]);
+  return batter;
+}
+
+function measureIngredients(ingredient, quantity) {
+  console.log(`Measuring ${quantity} cups of ${ingredient}`);
+  return {
+    ingredient,
+    quantity,
+  };
+}
+
+function mixIngredients(ingredients) {
+  console.log("Mixing ingredients together");
+  return {
+    mixture: ingredients,
+    consistency: "smooth",
+  };
+}
+
+function bakeBatter(batter) {
+  console.log("Baking the batter...");
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Baking complete!");
+      const bakedCake = {
+        batter,
+        status: "baked",
+      };
+      resolve(bakedCake);
+    }, 5000);
+  });
+}
+
+function frostCake(cake) {
+  if (cake.status != "baked") {
+    throw new Error("Cake is not baked!!! Gooey! Mush!");
+  }
+  console.log("Frosting the cake...");
+
+  (cake["status"] = "frosted"), (cake["decoration"] = "sprinkles");
+
+  const frostedCake = cake;
+  return frostedCake;
+}
+
+bakeCake();
