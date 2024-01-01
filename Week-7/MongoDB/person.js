202c78fd87a7f3611ec56693aa23e0aa9e97155b
Saber-Elsayed <alsayyedsabe@gmail.com>
2024-01-01 13:44:50 +0200

    mongoose exercises push data to database

--- Week-7/MongoDB/person.js
+++ Week-7/MongoDB/person.js
@@ -14,3 +14,20 @@ const personSchema = new Schema({
 const Person = mongoose.model("person", personSchema);
 
 let p1 = new Person({ firstName: "David", lastName: "Smith", age: 25 }); //purposefully ignoring the `address` field
+p1.save();
+
+let peoplePromise = Person.find({});
+
+peoplePromise
+  .then(function (people) {
+    console.log(people);
+  })
+  .catch((err) => console.log(err));
+
+let p2 = new Person({ firstName: "Shoo", lastName: "Bert", age: 50 });
+let p3 = new Person({ firstName: "Shoob", lastName: "Ert", age: 34 });
+let p4 = new Person({ firstName: "Sh", lastName: "Oobert", age: 10 });
+let p5 = new Person({ firstName: "Shoober", lastName: "T", age: 44 });
+
+let allTheShooberts = [p2, p3, p4, p5];
+allTheShooberts.forEach((s) => s.save());
