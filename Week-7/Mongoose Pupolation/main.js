9cbc1b943245b3aeee9d3aa1f196c7a1b6d29f8a
Saber-Elsayed <alsayyedsabe@gmail.com>
2024-01-03 10:41:04 +0200

    Reference IDs

--- Week-7/Mongoose Pupolation/main.js
+++ Week-7/Mongoose Pupolation/main.js
@@ -1,40 +1,64 @@
-//No data integrity
+// Mongoose setup
+const mongoose = require("mongoose");
 
-const bookSchema1 = new Schema({
-  title: String,
-  author: String,
-  reviews: [reviewSchema],
-});
-
-const reviewSchema1 = new Schema({
-  book: bookSchema,
-  reviewText: String,
-  critic: criticSchema,
-});
-
-const criticSchema1 = new Schema({
-  name: String,
-  reviews: [reviewSchema],
-});
-const Book = mongoose.model("Book", bookSchema);
-const Review = mongoose.model("Review", reviewSchema);
-const Critic = mongoose.model("Critic", criticSchema);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/peopleDB", {
+    useNewUrlParser: true,
+  })
+  .catch((err) => console.log(err));
+const Schema = mongoose.Schema;
 
 //data integrity
 
-const bookSchema2 = new Schema({
+const bookSchema = new Schema({
   title: String,
   author: String,
   reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }], //reference to an array of Review documents
 });
 
-const reviewSchema2 = new Schema({
+const reviewSchema = new Schema({
   book: { type: Schema.Types.ObjectId, ref: "Book" }, //reference to a Book document
   reviewText: String,
   critic: { type: Schema.Types.ObjectId, ref: "Critic" }, //reference to a Critic document
 });
 
-const criticSchema2 = new Schema({
+const criticSchema = new Schema({
   name: String,
   reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }], //reference to an array of Review documents
 });
+
+const Book = mongoose.model("Book", bookSchema);
+const Review = mongoose.model("Review", reviewSchema);
+const Critic = mongoose.model("Critic", criticSchema);
+
+let b1 = new Book({
+  title: "Name of the Wind",
+  author: "Patrick Rothfuss",
+  reviews: [],
+});
+
+let c1 = new Critic({
+  name: "William Jeffery",
+  reviews: [],
+});
+
+let r1 = new Review({
+  book: b1,
+  critic: c1,
+  reviewText: "Excellent Book",
+});
+
+b1.reviews.push(r1);
+c1.reviews.push(r1);
+
+b1.save();
+c1.save();
+r1.save();
+Book.find({})
+  .exec()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log("ERROR");
+  });
